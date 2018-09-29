d1e828db6f45cb119315fa5a8c2708cb7fe842da
Ranbo-20171103200 <1124280263@qq.com>
2018-09-29 09:50:40 +0800

    Rank
    排序

--- Rank/main.swift
+++ Rank/main.swift
@@ -7,6 +7,22 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var array = [11,3,53,21,34,12,34]
+for i in (0..<array.count) {
+    
+    for j in (i..<array.count - 1) {
+        
+        if array[j] > array[j + 1] {
+            
+            let tmp = array[j]
+            
+            array[j] = array[j + 1]
+            
+            array[j + 1] = tmp
+            
+        }
+        
+    }
+    
+}
+print(array)
